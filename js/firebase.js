bc40020f99f707a882f72db77a72f04ed85025f8
Tova2539 <tova2539@gmail.com>
2022-05-03 17:26:30 +1000

    Updated Contact form and T+C

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiH_Lp5OtAi5V8ciPep-qBVto-pYf-tb0",
+  authDomain: "halseon-website.firebaseapp.com",
+  projectId: "halseon-website",
+  storageBucket: "halseon-website.appspot.com",
+  messagingSenderId: "300703311898",
+  appId: "1:300703311898:web:55eb06aeb7eb10e75f49a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- mail.php
+++ mail.php
@@ -1,40 +1,49 @@
 <? php
+  echo '<script>console.log('hello1')</script></script>';
   $firstName = $_POST['firstName'];
   $surname = $_POST['surname'];
   $email = $_POST['email'];
   $phone = $_POST['phone'];
   $business = $_POST['business'];
   $message = $_POST['message'];
-  header('Content-Type: application/json');
+  echo '<script>console.log('hello2')</script></script>';
+
+  // header('Content-Type: application/json');
+
   if ($firstName === '') {
-    print json_encode(array('message' => 'First name cannot be empty', 'code' => 0));
+    // print json_encode(array('message' => 'First name cannot be empty', 'code' => 0));
     exit();
   }
   if ($surname === '') {
-    print json_encode(array('message' => 'Surname cannot be empty', 'code' => 0));
+    // print json_encode(array('message' => 'Surname cannot be empty', 'code' => 0));
     exit();
   }
   if ($email === '') {
-    print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+    // print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
     exit();
   } else {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
+      // print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
       exit();
     }
   }
   if ($phone === '') {
-    print json_encode(array('message' => 'Phone cannot be empty', 'code' => 0));
+    // print json_encode(array('message' => 'Phone cannot be empty', 'code' => 0));
     exit();
   }
   if ($business === '') {
-    print json_encode(array('message' => 'Business cannot be empty', 'code' => 0));
+    // print json_encode(array('message' => 'Business cannot be empty', 'code' => 0));
     exit();
   }
   $content = "From: $firstName $surname \nEmail: $email \nPhone: $phone \nBusiness: $business \nMessage: $message";
   $recipient = "zack@halseon.com";
-  $mailheader = "From: [HALSEON WEBSITE]$email \r\n";
-  mail($recipient, '[WEBSITE] Halseon Inquiry', $content, $mailheader) or die("Error!");
-  print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
+  $mailheader = "From:$email \r\n";
+  $retval = mail($recipient, '[WEBSITE] Halseon Inquiry', $content, $mailheader); // or die("Error!");
+  if ($retval == true) {
+    echo "success";
+  } else {
+    echo "failure";
+  }
+  // print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
   exit();
 ?>
